53aeab157de7fc12447d44148d98355ad3644d47
Robert Perez <31597452+robert9111@users.noreply.github.com>
2018-06-12 20:57:07 -0700

    Add files via upload

---
+++ mapsAPI.py
@@ -0,0 +1,45 @@
+#################################################
+# Extremely basic api call to google maps to get
+# and understanding of how things work
+#################################################
+
+
+# Import libraries needed to run parsing and requests
+import urllib.parse
+import requests
+
+# Main URL where we will be calling on the request
+main_api = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+# While true keeps the program alive even after 1st run through
+while True:
+    # We ask for user input for where they would like information on
+    address = input('Enter a zip code: ')
+
+    # Provide a quit option for user
+    if address == 'quit' or address == 'q':
+        break
+
+    # This is where we will append our user input into the request
+    url = main_api + urllib.parse.urlencode({'address': address})
+    # print(url)
+
+    # These lines provide the constructed URL where we will be finding all the data
+    # Left here in case we want to see the full URL and grab MORE information for the user
+    # json_data = requests.get(url).json()
+    # print(json_data)
+
+    # This line lets us know if our query worked or not
+    json_status = json_data['status']
+    # print('API Status: ' + json_status)
+
+    # Main loop which lets us 1. Verify status 2. parse through data with a loop 3. Display them to the user
+    if json_status == 'OK':
+        for each in json_data['results'][0]['address_components']:
+            print(each['long_name'])
+
+    # This line just shows our address formatted, the zip code we looked up
+        formatted_address = json_data['results'][0]['formatted_address']
+        print()
+        print('Zip code is: ' + formatted_address)
+
